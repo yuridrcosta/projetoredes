a0226cc0a85ac3a397c1a2070957ab3073ec5d43
João Paulo Agostinho <43277315+joaopauloag@users.noreply.github.com>
2021-09-15 11:27:32 -0300

    Atualizando print's do cliente

--- client.py
+++ client.py
@@ -13,14 +13,14 @@ ENCODE_FORMAT = 'utf-8'
 def main():
     address = (IP, PORT)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print(f'Conectado ao servidor {address}')
     client_socket.connect(address)
+    print(f'\nConectado ao servidor {address}')
 
     # Echo
     while True:
         # Loop de leitura do protocolo
         full_protocol = ""
-        text = input("Informe o protocolo ou digite '!SAIR' para desconectar: \n >")
+        text = input("\nInforme o protocolo ou digite '!SAIR' para desconectar: \n >")
         text = re.sub("\n",'',text)
         if (text == "!SAIR"):
             client_socket.send(text.encode(ENCODE_FORMAT))
@@ -37,17 +37,17 @@ def main():
         # O servidor irá retornar o status da operação
         status_response = client_socket.recv(DATA_SIZE).decode(ENCODE_FORMAT)
         status_response = status_response.rstrip()
-        print(f'[Resposta] {status_response}')
+        print(f'\n[Resposta] {status_response}')
         if status_response.split(' ')[0] == '200':
             # Código 200 representa que ocorreu tudo certo e a imagem será enviada
-            filename = input("Salvar imagem como: ")
+            filename = input("\nSalvar imagem como: ")
             img_file = open(f'{filename}.png','wb')
             # O servidor envia uma imagem que é maior que a capacidade de envio por meio
             # dos sockets (2048 bytes), por isso, quebramos a imagem em pedaços e enviamos.
             img_chunk = client_socket.recv(100000)
             img_file.write(img_chunk)
             img_file.close()
-    print('Conexão finalizada')
+    print('\nConexão finalizada')
 
 
 if __name__ == '__main__':
